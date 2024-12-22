6f183e8b28c507be89241a0228d9034e60a3b2b1
muhamad fajar <mfajar2123@gmail.com>
2024-12-22 17:20:57 +0700

    Fix MongoDB deprecation warning and missing route

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
+  
 
 // Routes
 const itemsRoutes = require('./routes/items');
